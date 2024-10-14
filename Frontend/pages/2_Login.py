5d857e3b1d473192a66105efd16fec14b3579af0
Nikhil <nikhil.nakashe@greatmanagerinstitute.com>
2024-10-14 18:10:49 +0530

    Changes in data type

--- Frontend/pages/2_Login.py
+++ Frontend/pages/2_Login.py
@@ -5,7 +5,7 @@ import os
 
 # Load environment variables
 host = os.getenv("DB_HOST")
-port = int(os.getenv("DB_PORT"))
+port = os.getenv("DB_PORT")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_NAME")
